76d4a9948aeadbcae97a2eb67d30517801f4d758
Rockett611 <r.podnajmer@gmail.com>
2019-02-26 10:04:30 +0100

    premier add question html

--- server.py
+++ server.py
@@ -1,13 +1,16 @@
 from flask import Flask, render_template
-app = Flask(__name__)
+
+app = Flask(__name__,template_folder = 'Templates')
+
 
 @app.route('/')
-
 def route_index():
-    return render_template('/main_page.html')
+    return render_template('main_page.html')
 
-"""if __name__ == "__main__":
-    app.run(
-        debug=True,
-        port=5000
-    )"""
+
+@app.route('/add_question')
+def route_add_question():
+    return render_template('add_question.html')
+
+if __name__ == "__main__":
+    app.run()
