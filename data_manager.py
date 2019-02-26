60b232c007527404ec0ad12b468daa92cee9aef8
KamoOr13 <pawelnowak13@gmail.com>
2019-02-26 12:24:43 +0100

    Deleted useless function

--- data_manager.py
+++ data_manager.py
@@ -1,10 +1,8 @@
 import csv
 
-def transform_questions_into_list():
-    with open('sample_data/question.csv', 'r') as file:
-        reader = csv.reader(file)
-        my_list = list(reader)
-        del my_list[0]
-        return my_list
+def generate_id(question_list):
+    id = None
+    for i in question_list:
+        id = int(i[0])
+    return id+1
 
-print(transform_questions_into_list())
