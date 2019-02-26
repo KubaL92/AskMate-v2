24189d38e75960436c8df70166d18b268e10c428
KamoOr13 <pawelnowak13@gmail.com>
2019-02-26 13:15:21 +0100

    Added function returning questions titles

--- data_manager.py
+++ data_manager.py
@@ -24,4 +24,9 @@ def csv_to_list(file_path: str) -> list:
         # return the temporary list
                 return user_stories
 
+def get_titles(user_stories: list) -> list:
+    list_of_titles = []
+    for i in user_stories:
+        list_of_titles.append(i['title'])
+    return list_of_titles
 
