10f01a5cd1367ec8b02e9c7435471e69786e6e9b
Rockett611 <r.podnajmer@gmail.com>
2019-02-26 14:17:54 +0100

    reading list of titles questions (to change to reading titles form dict) in def route_index

--- server.py
+++ server.py
@@ -1,11 +1,14 @@
 from flask import Flask, render_template
+import data_manager
 
 app = Flask(__name__,template_folder = 'Templates')
 
 
 @app.route('/')
 def route_index():
-    return render_template('main_page.html')
+    user_questions = data_manager.get_titles(data_manager.csv_to_list('sample_data/question.csv'))
+
+    return render_template('main_page.html', user_questions=user_questions)
 
 
 @app.route('/add_question')
